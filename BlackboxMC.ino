f028fa84253108e39f6e8138f419e70a98dcbc9f
MatthewMMontoya <montoyam.matthew@gmail.com>
2024-07-27 10:11:33 -0600

    cleaning up comments

--- BlackboxMC.ino
+++ BlackboxMC.ino
@@ -8,7 +8,7 @@
 #include "SD.h"
 #include "SPI.h"
 #include <SPIFFS.h>
-//MACROS
+// MACROS
 #define RX_PIN 33
 #define TX_PIN 27
 #define GPS_BAUD_RATE 9600
@@ -20,50 +20,57 @@
 #define BMP_MOSI 18
 #define DEBUG true
 
-//UART COMM (Talking to transmit chip)
+// UART COMM (Talking to transmit chip)
 HardwareSerial Comm(2);
 
-String comm_msg;  //incoming transmitMC msgs
+String comm_msg; // incoming transmitMC msgs
 
-//GPS
+// GPS
 #define GPSECHO false
 #define GPSSerial Serial1
 Adafruit_GPS GPS(&GPSSerial);
 
-enum AvGPSState { AV_NOFIX = 14,
-                  AV_FIX = 24 } av_gps_state;
+enum AvGPSState
+{
+  AV_NOFIX = 14,
+  AV_FIX = 24
+} av_gps_state;
 
-//BNO
+// BNO
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 float pitch;
 float yaw;
 float roll;
 
-  //BMP Stuff
-  Adafruit_BMP3XX bmp;
+// BMP Stuff
+Adafruit_BMP3XX bmp;
 double press;
 double temp;
 double alt;
 
-//timers
+// timers
 unsigned long gps_send_timer = millis();
 unsigned long bno_timer;
 unsigned long bmp_timer;
 unsigned long currentTime;
 
-void init_gps() {
+void init_gps()
+{
   // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-  if (!GPS.begin(GPS_BAUD_RATE)) {
+  if (!GPS.begin(GPS_BAUD_RATE))
+  {
     Serial.println("Failed GPS begin");
-    Comm.println("FAILED Avionics GPS begin");  // send to transmit code
-  } else {
+    Comm.println("FAILED Avionics GPS begin"); // send to transmit code
+  }
+  else
+  {
     Serial.println("Success GPS begin");
     Comm.println("SUCCESS Avionics GPS begin");
   }
 
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);  // 1 Hz update rate
-  GPS.sendCommand(PGCMD_ANTENNA);             //antenna status updates
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  GPS.sendCommand(PGCMD_ANTENNA);            // antenna status updates
   delay(1000);
   // Ask for firmware version
   GPSSerial.println(PMTK_Q_RELEASE);
@@ -72,28 +79,38 @@ void init_gps() {
 //
 // SD FUNCS
 //
-void sd_setup() { // FIXME RETURN the SD object to be used
+void sd_setup()
+{
   Serial.println("Sensor Setup");
   // SD setup
-  if (!SD.begin(CS)) {
+  if (!SD.begin(CS))
+  {
     Serial.println("Card Mount Failed");
     return;
   }
   uint8_t cardType = SD.cardType();
 
-  if (cardType == CARD_NONE) {
+  if (cardType == CARD_NONE)
+  {
     Serial.println("No SD card attached");
     return;
   }
 
   Serial.print("SD Card Type: ");
-  if (cardType == CARD_MMC) {
+  if (cardType == CARD_MMC)
+  {
     Serial.println("MMC");
-  } else if (cardType == CARD_SD) {
+  }
+  else if (cardType == CARD_SD)
+  {
     Serial.println("SDSC");
-  } else if (cardType == CARD_SDHC) {
+  }
+  else if (cardType == CARD_SDHC)
+  {
     Serial.println("SDHC");
-  } else {
+  }
+  else
+  {
     Serial.println("UNKNOWN");
   }
 
@@ -102,43 +119,57 @@ void sd_setup() { // FIXME RETURN the SD object to be used
   Serial.println("SD setup complete");
   Comm.println("SD setup complete");
 }
-void writeFile(fs::FS &fs, const char * path, const char * message){
+void writeFile(fs::FS &fs, const char *path, const char *message)
+{
   Comm.printf("Writing file: %s\n", path);
   Serial.printf("Writing file: %s\n", path);
-  
+
   File file = fs.open(path, FILE_WRITE);
-  if(!file){
+  if (!file)
+  {
     Comm.println("Failed to open file for writing");
     Serial.println("Failed to open file for writing");
     return;
   }
-  if(file.print(message)){
+  if (file.print(message))
+  {
     Comm.println("File written");
     Serial.println("File written");
-  } else {
+  }
+  else
+  {
     Comm.println("Write failed");
     Serial.println("Write failed");
   }
-  file.close();   
-  delay(50);  // ensures file closes               
+  file.close();
+  delay(50); // ensures file closes
 }
 
-void appendFile(fs::FS &fs, const char * path, const char * message){
-  if(DEBUG)Comm.printf("Appending to file: %s\n", path);
-  if(DEBUG)Serial.printf("Appending to file: %s\n", path);
-  
+void appendFile(fs::FS &fs, const char *path, const char *message)
+{
+  if (DEBUG)
+    Comm.printf("Appending to file: %s\n", path);
+  if (DEBUG)
+    Serial.printf("Appending to file: %s\n", path);
+
   File file = fs.open(path, FILE_APPEND);
-  if(!file){
-     Comm.println("Failed to open file for appending");
-     Serial.println("Failed to open file for appending");
-     return;
+  if (!file)
+  {
+    Comm.println("Failed to open file for appending");
+    Serial.println("Failed to open file for appending");
+    return;
   }
-  if(file.print(message)){
-     if(DEBUG)Comm.println("Message appended");
-     if(DEBUG)Serial.println("Message appended");
-  } else {
-     Comm.println("Append failed");
-     Serial.println("Append failed");
+  if (file.print(message))
+  {
+    if (DEBUG)
+      Comm.println("Message appended");
+    if (DEBUG)
+      Serial.println("Message appended");
+  }
+  else
+  {
+    Comm.println("Append failed");
+    Serial.println("Append failed");
   }
   file.close();
   delay(50); // ensures file closes
@@ -148,53 +179,65 @@ void appendFile(fs::FS &fs, const char * path, const char * message){
 //
 // SPIFFS
 //
-void writeFile(const char * path, const char * message) {
-    File file = SPIFFS.open(path, FILE_APPEND);
-    if (!file) {
-        Serial.println("Failed to open file for writing");
-        return;
-    }
-    if (file.print(message)) {
-        Serial.println("File written successfully");
-    } else {
-        Serial.println("Write failed");
-    }
-    file.close();
+void writeFile(const char *path, const char *message)
+{
+  File file = SPIFFS.open(path, FILE_APPEND);
+  if (!file)
+  {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if (file.print(message))
+  {
+    Serial.println("File written successfully");
+  }
+  else
+  {
+    Serial.println("Write failed");
+  }
+  file.close();
 }
 
-void logData() {
-    // Replace these variables with actual sensor readings
-    float temperature = 25.0;
-    float pressure = 1013.25;
-    float altitude = 150.0;
+void logData()
+{
+  // Replace these variables with actual sensor readings
+  float temperature = 25.0;
+  float pressure = 1013.25;
+  float altitude = 150.0;
 
-    String dataString = "Temperature: " + String(temperature) + ", Pressure: " + String(pressure) + ", Altitude: " + String(altitude) + "\n";
-    writeFile("/datalog.txt", dataString.c_str());
+  String dataString = "Temperature: " + String(temperature) + ", Pressure: " + String(pressure) + ", Altitude: " + String(altitude) + "\n";
+  writeFile("/datalog.txt", dataString.c_str());
 }
 
-void readFile(const char * path) {
-    File file = SPIFFS.open(path, FILE_READ);
-    if (!file) {
-        Serial.println("Failed to open file for reading");
-        return;
-    }
-    Serial.println("Reading from file:");
-    while (file.available()) {
-        Serial.write(file.read());
-    }
-    file.close();
+void readFile(const char *path)
+{
+  File file = SPIFFS.open(path, FILE_READ);
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+  Serial.println("Reading from file:");
+  while (file.available())
+  {
+    Serial.write(file.read());
+  }
+  file.close();
 }
-
 
 //
 // Sensor Functions
 //
-void bmp_setup() { 
-  while (!Serial) delay(10);
+void bmp_setup()
+{
+  while (!Serial)
+    delay(10);
 
-  if (!bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {
+  if (!bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI))
+  {
     Serial.println("No BMP detected \n");
-    while (1);
+    while (1)
+      ;
   }
 
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
@@ -205,20 +248,25 @@ void bmp_setup() {
   Serial.println("Adafruit BMP3XX Initiated \n");
 }
 
-void bno_setup() {
-  while (!Serial) delay(10);
+void bno_setup()
+{
+  while (!Serial)
+    delay(10);
 
   Serial.println("BNO Setup beginning\n");
 
-  if (!bno.begin()) {
+  if (!bno.begin())
+  {
     Serial.println("No BNO detected, check wiring!\n");
-    while (1);
+    while (1)
+      ;
   }
 
   delay(50);
 
-  //Displays details and calibration values for BNO
-  if(DEBUG){
+  // Displays details and calibration values for BNO
+  if (DEBUG)
+  {
     displayBNODetails();
     displayCalStatus();
     Serial.println("");
@@ -226,9 +274,11 @@ void bno_setup() {
   Serial.println("BNO Initiated \n");
 }
 
-//BMP take measurements
-void bmp_test() {
-  if (!bmp.performReading()) {
+// BMP take measurements
+void bmp_test()
+{
+  if (!bmp.performReading())
+  {
     Serial.println("Failed to perform reading :(");
     return;
   }
@@ -248,14 +298,16 @@ void bmp_test() {
   delay(2000);
 }
 
-void getBMP() {
+void getBMP()
+{
   temp = (bmp.temperature);
   press = (bmp.pressure / 100.0);
   alt = (bmp.readAltitude(SEALEVELPRESSURE_HPA));
 }
 
-//BNO sensor details for debug
-void displayBNODetails(void) {
+// BNO sensor details for debug
+void displayBNODetails(void)
+{
   sensor_t sensor;
   bno.getSensor(&sensor);
   Serial.println("------------------------------------");
@@ -279,7 +331,8 @@ void displayBNODetails(void) {
   delay(500);
 }
 
-void displayCalStatus(void) {
+void displayCalStatus(void)
+{
   /* Get the four calibration values (0..3) */
   /* Any sensor data reporting 0 should be ignored, */
   /* 3 means 'fully calibrated" */
@@ -289,7 +342,8 @@ void displayCalStatus(void) {
 
   /* The data should be ignored until the system calibration is > 0 */
   Serial.print("\t");
-  if (!system) {
+  if (!system)
+  {
     Serial.print("! ");
   }
 
@@ -304,21 +358,23 @@ void displayCalStatus(void) {
   Serial.print(mag, DEC);
 }
 
-void getBNO() {
+void getBNO()
+{
   sensors_event_t event;
   bno.getEvent(&event);
   pitch = event.orientation.y;
   yaw = event.orientation.x;
   roll = event.orientation.z;
-  //Assuming breadboard orientation (BNO above ESP)
-  // X = Yaw, Y = Pitch, Z = Roll
+  // Assuming breadboard orientation (BNO above ESP)
+  //  X = Yaw, Y = Pitch, Z = Roll
 }
 
 // End Sensor functions
 
 // arduino setup hook
-void setup() {
-  //Serial Initializations
+void setup()
+{
+  // Serial Initializations
   Serial.begin(115200);
   delay(1000);
   Serial.println("Initializing Black Box Boot Sequence");
@@ -327,119 +383,137 @@ void setup() {
   Comm.begin(115200, SERIAL_8N1, RX_PIN, TX_PIN);
   Serial.println("UART COMM (communication line with transmit MC) Initialized");
 
-  //SPIFFS
-  if (!SPIFFS.begin(true)) {
+  // SPIFFS
+  if (!SPIFFS.begin(true))
+  {
     Serial.println("An Error has occurred while mounting SPIFFS");
   }
-  else{
+  else
+  {
     Serial.println("SPIFFS mounted successfully");
     // readFile("/datalog.txt");
   }
-  //GPS
+  // GPS
   av_gps_state = AV_NOFIX;
   Serial.println("Initializing GPS");
   init_gps();
-  if(GPS.fix){
+  if (GPS.fix)
+  {
     String s = "STATE," + String(AV_FIX);
     av_gps_state = AV_FIX;
     Comm.println(s);
   }
   Serial.println("GPS Initialized");
 
-  // sd_setup(); //FIXME // fix function before uncommenting //probably have to return a reference to SD
   bmp_setup();
   bno_setup();
   bno_timer = millis();
   bmp_timer = millis();
 
-    if(!SD.begin(CS)){ // doing sd setup here rather than in the function
-        Comm.println("Card Mount Failed");
-        Serial.println("Card Mount Failed");
-        return;
-    }
-    uint8_t cardType = SD.cardType();
+  if (!SD.begin(CS))
+  {
+    Comm.println("Card Mount Failed");
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
 
-    if(cardType == CARD_NONE){
-        Comm.println("No SD card attached");
-        Serial.println("No SD card attached");
-        return;
-    }
+  if (cardType == CARD_NONE)
+  {
+    Comm.println("No SD card attached");
+    Serial.println("No SD card attached");
+    return;
+  }
 
-    writeFile(SD, "/datalog.csv", "Dawson,is,the,goat\n"); //test file
-    appendFile(SD, "/datalog.csv", "the,greatest,of,all,time\n");
+  writeFile(SD, "/datalog.csv", "Dawson,is,the,goat\n"); // test file
+  appendFile(SD, "/datalog.csv", "the,greatest,of,all,time\n");
 
-    writeFile(SD,"/GPS.csv","GPS,Hour:Min:Sec,Latitude,Longitude,Speed,Altitude,Geoid Height,ENDDATA\n");
-    writeFile(SD,"/BMP.csv","BMP,Temperature,Pressure,Altitude,ENDDATA\n");
-    writeFile(SD,"/BNO.csv","BNO,Pitch,Roll,Yaw,ENDDATA\n");
+  writeFile(SD, "/GPS.csv", "GPS,Hour:Min:Sec,Latitude,Longitude,Speed,Altitude,Geoid Height,ENDDATA\n");
+  writeFile(SD, "/BMP.csv", "BMP,Temperature,Pressure,Altitude,ENDDATA\n");
+  writeFile(SD, "/BNO.csv", "BNO,Pitch,Roll,Yaw,ENDDATA\n");
 
   Serial.println("Setup Fully Completed");
 }
 
 // arduino loop hook
-void loop(){
+void loop()
+{
   // read data from the GPS in the 'main loop'
   char c = GPS.read();
 
   // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
+  if (GPS.newNMEAreceived())
+  {
     // Serial.print(GPS.lastNMEA()); // newNMEAreceived() flag to false
     GPS.lastNMEA();
-    if (!GPS.parse(GPS.lastNMEA())) //newNMEAreceived() flag to false
-      return; // we can fail to parse a sentence in which case we should just wait for another
+    if (!GPS.parse(GPS.lastNMEA())) // newNMEAreceived() flag to false
+      return;                       // we can fail to parse a sentence in which case we should just wait for another
   }
 
   comm_msg = "";
-  if(Comm.available()>0){ //read from blackbox
+  if (Comm.available() > 0)
+  { // read from blackbox
     comm_msg = Comm.readStringUntil('\n');
-    if(comm_msg.equals("AVFIX\r")){  
-      av_gps_state = AV_FIX;  // Received an ACK: Stop sending STATE,AV_FIX to Groundstation
+    if (comm_msg.equals("AVFIX\r"))
+    {
+      av_gps_state = AV_FIX; // Received an ACK: Stop sending STATE,AV_FIX to Groundstation
       delay(50);
     }
-
-  }  
-  if(GPS.fix && av_gps_state == AV_NOFIX && (millis()-gps_send_timer > 2000)){
-    String s = "STATE," + String(AV_FIX); //sending STATE,AV_FIX to Groundstation
-    //intentionally not setting av_gps_state, waiting for an ACK
+  }
+  if (GPS.fix && av_gps_state == AV_NOFIX && (millis() - gps_send_timer > 2000))
+  {
+    String s = "STATE," + String(AV_FIX); // sending STATE,AV_FIX to Groundstation
+    // intentionally not setting av_gps_state, waiting for an ACK
     Serial.println(s);
     Comm.println(s);
     gps_send_timer = millis();
   }
 
   currentTime = millis();
-  if(GPS.fix && (millis()-gps_send_timer > 1000)){
+  if (GPS.fix && (millis() - gps_send_timer > 1000))
+  {
     String gps_ = "GPS,";
-    String gps_data = gps_ + 
-                      String(GPS.hour) + ":" + String(GPS.minute) + ":" + String(GPS.seconds) + "," + 
-                      String(GPS.latitude,4) + GPS.lat + "," + 
-                      String(GPS.longitude,4) + GPS.lon +  "," + 
-                      String(GPS.speed) + "knots," + 
-                      String(GPS.altitude) + "meters," + 
-                      String(GPS.geoidheight) + "meters," + 
+    String gps_data = gps_ +
+                      String(GPS.hour) + ":" + String(GPS.minute) + ":" + String(GPS.seconds) + "," +
+                      String(GPS.latitude, 4) + GPS.lat + "," +
+                      String(GPS.longitude, 4) + GPS.lon + "," +
+                      String(GPS.speed) + "knots," +
+                      String(GPS.altitude) + "meters," +
+                      String(GPS.geoidheight) + "meters," +
                       "ENDDATA\n";
     Serial.println(gps_data);
-    Comm.println(gps_data); 
-    appendFile(SD,"/GPS.csv",gps_data.c_str());
-    
+    Comm.println(gps_data);
+    appendFile(SD, "/GPS.csv", gps_data.c_str());
+
     gps_send_timer = millis();
   }
-  
 
-  if ((currentTime - bmp_timer) > 1000) {
+  if ((currentTime - bmp_timer) > 1000)
+  {
     getBMP();
-    //String s = "BMP," + "Temperature," + "Pressure," + "Altitude," + "ENDDATA";
+    // String s = "BMP," + "Temperature," + "Pressure," + "Altitude," + "ENDDATA";
     String s = "BMP," + String(temp) + "," + String(press) + "," + String(alt) + ",ENDDATA\n";
-    if(DEBUG){Serial.println(s); Serial.println("");}
-    Comm.println(s); //send to transmitMC
+    if (DEBUG)
+    {
+      Serial.println(s);
+      Serial.println("");
+    }
+    Comm.println(s);                       // send to transmitMC
     appendFile(SD, "/BMP.csv", s.c_str()); // log bmp data to sd card
-    logData(); // log bmp to ESP32 storage
+    logData();                             // log bmp to ESP32 storage
     bmp_timer = currentTime;
   }
 
-  if ((currentTime - bno_timer) > 1000) {
+  if ((currentTime - bno_timer) > 1000)
+  {
     getBNO();
     String st = "BNO," + String(pitch) + "," + String(roll) + "," + String(yaw) + ",ENDDATA\n";
-    if(DEBUG){Serial.println(st); Serial.println("");}
-    Comm.println(st); //send to transmitMC
+    if (DEBUG)
+    {
+      Serial.println(st);
+      Serial.println("");
+    }
+    Comm.println(st);                       // send to transmitMC
     appendFile(SD, "/BNO.csv", st.c_str()); // log bno data to sd card
     bno_timer = currentTime;
   }
